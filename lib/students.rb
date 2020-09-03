611b620694cad1f48c71cf883b8d2882cdeb4df3
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-03 16:46:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,12 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN (gpa) FROM students AS lowest_student_gpa;
-MIN (gpa) "
+  "SELECT MIN (gpa) FROM students AS lowest_student_gpa;"
 end
 
 def average_student_gpa
-  "SELECT AVG (gpa) FROM students AS average_student_gpa"
+  "SELECT AVG (gpa) FROM students AS average_student_gpa;"
 end
 
 def total_tardies_for_all_students
